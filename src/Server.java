9bb2fb7d9ef5312d5c1b4d83dd89062e403a7224
Matty <matty258987@gmail.com>
2022-05-13 14:16:15 +0100

    fix imports to be proper

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
