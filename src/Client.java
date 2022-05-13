e7b550e19840c378b771d192a6846e674e33ba5a
Matty <matty258987@gmail.com>
2022-05-13 14:13:43 +0100

    fix imports for assignment submission

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Client {
     public static void main(String[] args) throws IOException {
