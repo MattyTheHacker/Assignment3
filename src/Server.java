093fe4a71caf80bc3cafb1c285081574f2fab851
Matty <matty258987@gmail.com>
2022-05-15 21:03:30 +0100

    add try-with-resources

--- src/Server.java
+++ src/Server.java
@@ -4,21 +4,22 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(Credentials.PORT);
-        System.out.println("Server is running");
-        int clientId = 0;
-        Database db = new Database();
-        if (db.establishDBConnection()) {
-            System.out.println("Server is now connected to DB");
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                clientId++;
-                System.out.println("Client " + clientId + " connected with IP " + clientSocket.getInetAddress().getHostAddress());
-                Thread client = new Thread(new ClientHandler(clientSocket, clientId, db));
-                client.start();
+        try (ServerSocket serverSocket = new ServerSocket(Credentials.PORT)) {
+            System.out.println("Server is running");
+            int clientId = 0;
+            Database db = new Database();
+            if (db.establishDBConnection()) {
+                System.out.println("Server is now connected to DB");
+                while (true) {
+                    Socket clientSocket = serverSocket.accept();
+                    clientId++;
+                    System.out.println("Client " + clientId + " connected with IP " + clientSocket.getInetAddress().getHostAddress());
+                    Thread client = new Thread(new ClientHandler(clientSocket, clientId, db));
+                    client.start();
+                }
+            } else {
+                System.out.println("DB connection fail, stopping.");
             }
-        } else {
-            System.out.println("DB connection fail, stopping.");
         }
     }
 }
